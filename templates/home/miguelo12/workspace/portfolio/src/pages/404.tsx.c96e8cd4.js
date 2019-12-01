583d3b44ff1236ec246a220ebf94a67c9ded77ce
miguelo12 <mi.sanchezp12@gmail.com>
2019-12-01 11:43:19 -0300

    Updates

--- templates/home/miguelo12/workspace/portfolio/src/pages/404.tsx.c96e8cd4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{275:function(n,e,t){"use strict";t.r(e);var a=t(0),i=t.n(a);e.default=function(){return i.a.createElement("div",{style:{background:"#fff",padding:24,minHeight:380}},i.a.createElement("h1",null,"404 - ouh! :("))}}}]);
