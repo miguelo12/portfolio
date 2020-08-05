f08d31327fd6526f6e0b2fc17d7441dfbe194c03
miguelo12 <mi.sanchezp12@gmail.com>
2020-08-04 20:44:33 -0400

    Updates

---
+++ docs/_next/static/06wAxeCU9jU0VzjjRe6Wm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002Ff5496cf10645a0469b76b260aba77abf7c6b4b0a.337710fcacd49a59d6f1.js","static\u002Fcss\u002F154642269a26c1375447.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
